3efab6cb13c1648b266e627f4bfdfa32e609867d
Taylor Lapeyre <taylorlapeyre@gmail.com>
2012-12-12 11:41:09 -0600

    change name..

--- use_banks.rb
+++ use_banks.rb
@@ -3,7 +3,7 @@ require './Banks'
 chase = Bank.new("Chase Bank")
 
 chase.add_account("checking", "Taylor Lapeyre", "0")
-chase.add_account("savings", "Katie Ball", "100")
+chase.add_account("savings", "Scrooge McDuck", "100")
 
 chase.get_account_by_id(1).deposit(10)
 chase.get_account_by_id(1).to_s
