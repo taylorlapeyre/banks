acc614321dcaeda63ab62ffd3ad07f88bca6c017
Taylor Lapeyre <taylorlapeyre@gmail.com>
2012-12-12 11:33:39 -0600

    add example files

---
+++ use_banks.rb
@@ -0,0 +1,13 @@
+require './Banks'
+
+chase = Bank.new("Chase Bank")
+
+chase.add_account("checking", "Taylor Lapeyre", "0")
+chase.add_account("savings", "Katie Ball", "100")
+
+chase.get_account_by_id(1).deposit(10)
+chase.get_account_by_id(1).to_s
+
+chase.save_bank_info_to_file("chase.bank")
+
+`cat chase.bank`
